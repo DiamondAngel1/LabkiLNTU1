08de8087e3d0b1936828bde3f2d2eaf4baba805c
DiamondAngel1 <tararukolga78@gmail.com>
2026-01-06 16:53:08 +0200

    lab13

---
+++ Laboratorna13/lab13.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+bool IsPalindrome(string s) {
+    int left = 0;
+    int right = s.length() - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    string text1 = "Hello";
+    string text2 = "World";
+    cout << "Перший рядок: " << text1 << endl;
+    cout << "Другий рядок: " << text2 << endl;
+
+    string combined = text1 + " " + text2;
+    cout << "Після конкатенації: " << combined << endl;
+
+    int length = combined.length();
+    cout << "Довжина рядка: " << length << endl;
+
+    char ch = combined[1]; 
+    cout << "Символ за індексом 1: " << ch << endl;
+
+    combined[0] = 'h';
+    cout << "Рядок після зміни символа: " << combined << endl;
+
+    string s;
+    cout << "Введіть рядок для перевірки паліндрому: ";
+    cin >> s;
+
+    if (IsPalindrome(s)) {
+        cout << "це паліндром" << endl;
+    }
+    else {
+        cout << "це не паліндром" << endl;
+    }
+
+    return 0;
+}
